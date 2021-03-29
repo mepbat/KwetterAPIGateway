4dd54239f03fe2789417748531706172bb186a8b
i383656 <s.westerburger@student.fontys.nl>
2021-03-29 10:47:31 +0200

    initial commit

---
+++ src/main/java/fontys/ict/kwetter/kwetterapigateway/KwetterApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package fontys.ict.kwetter.kwetterapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwetterApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwetterApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fontys/ict/kwetter/kwetterapigateway/KwetterApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package fontys.ict.kwetter.kwetterapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwetterApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
